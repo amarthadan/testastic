4a3058712415b21eb99f7381847b90af7d31791d
Michal Kimle <kimle.michal@gmail.com>
2020-03-31 21:55:41 +0200

    Add dummy home page
    Add Blueprint component library

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {H1, Button, H3} from '@blueprintjs/core'
+
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <H1>Welcome to Testastic!</H1>
+      <H3>Start by creating a new test</H3>
+      <Button large intent="primary" icon="add" />
+    </div>
+  )
+}
+
+export default Home
