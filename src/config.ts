a9b2aac192e80480b958ee9627e765e1011b1414
Michal Kimle <kimle.michal@gmail.com>
2020-05-02 11:28:33 +0200

    Add development Firebase environment

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,26 @@
 import firebaseDB from './database'
 import * as firebaseui from 'firebaseui'
 
-export const firebase = {
-  apiKey: 'AIzaSyB9FOgmnux53_L5NLARM03Dl2Cn_j7VYAk',
-  authDomain: 'testastic-9508b.firebaseapp.com',
-  databaseURL: 'https://testastic-9508b.firebaseio.com',
-  projectId: 'testastic-9508b',
-  storageBucket: 'testastic-9508b.appspot.com',
-  messagingSenderId: '103406953022',
-  appId: '1:103406953022:web:f4e51c60a18c437e71a76f',
-}
+export const firebase =
+  process.env.FIREBASE_ENV === 'production'
+    ? {
+        apiKey: 'AIzaSyB9FOgmnux53_L5NLARM03Dl2Cn_j7VYAk',
+        authDomain: 'testastic-9508b.firebaseapp.com',
+        databaseURL: 'https://testastic-9508b.firebaseio.com',
+        projectId: 'testastic-9508b',
+        storageBucket: 'testastic-9508b.appspot.com',
+        messagingSenderId: '103406953022',
+        appId: '1:103406953022:web:f4e51c60a18c437e71a76f',
+      }
+    : {
+        apiKey: 'AIzaSyCtwqma5Vmb5xCYAusq1o2_7M9qtiT686o',
+        authDomain: 'testastic-staging.firebaseapp.com',
+        databaseURL: 'https://testastic-staging.firebaseio.com',
+        projectId: 'testastic-staging',
+        storageBucket: 'testastic-staging.appspot.com',
+        messagingSenderId: '426164012322',
+        appId: '1:426164012322:web:e6724fd76e40f39b07517b',
+      }
 
 type SignInSuccessWithAuthResult = (authResult: unknown) => boolean
 
